1654aa71bdd19c59a4d2d50ba09036a00606cd33
Harry Tunstall <43201967+htunstall@users.noreply.github.com>
2019-01-09 09:15:25 +0000

    Delete setup.py
    superfluous

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(name='danmachi-builder',
-      version='1.0',
-	  description='An Encounter Builder for DanMachi',
-	  author='Flying Odobenus / Harry',
-      author_email='flying.odobenus+DanMachiDnD@gmail.com',
-      packages=[],
-      )
